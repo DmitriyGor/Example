07d73483f5f5fca1576eb675cf24bfb870e28ddb
Dmitriy <79133850099@yandex.ru>
2023-11-23 00:01:32 +0700

    Example003 Sum Random

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,6 +1,10 @@
-﻿// Суммма переменных А и В
-int numberA = 31;
-int numberB = 5;
+﻿int numberA = new Random().Next(1, 10); //  1 2 3... 9
+Console.Write("число А =  ");
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10); //  1 2 3... 9
+Console.Write("число В =  ");
+Console.WriteLine(numberB);
+Console.Write("сумма  =  ");
 int result = numberA + numberB;
 Console.WriteLine(result);
 
